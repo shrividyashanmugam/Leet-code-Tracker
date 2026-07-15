a0586a8dd0eaa3ee2e10b873b70ec81d977829b7
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:06:56 +0530

    Create 570-ManagersWithAtLeast5DirectReports

---
+++ 570-ManagersWithAtLeast5DirectReports/570-ManagersWithAtLeast5DirectReports.sql
@@ -0,0 +1,10 @@
+-- Last updated: 7/15/2026, 2:06:56 PM
+# Write your MySQL query statement below
+SELECT E1.name
+FROM Employee E1
+JOIN (
+    SELECT managerId, COUNT(*) AS directReports
+    FROM Employee
+    GROUP BY managerId
+    HAVING COUNT(*) >= 5
+) E2 ON E1.id = E2.managerId;
