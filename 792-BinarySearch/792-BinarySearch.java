158396d3c10faf2016b6bc2bbf47b708f1f623e5
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:06:25 +0530

    Create 792-BinarySearch

---
+++ 792-BinarySearch/792-BinarySearch.java
@@ -0,0 +1,21 @@
+// Last updated: 7/15/2026, 2:06:25 PM
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
