84f843cf42e99cadaacf410ec312f5e56f66f06b
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:07:42 +0530

    Create 242-ValidAnagram

---
+++ 242-ValidAnagram/242-ValidAnagram.java
@@ -0,0 +1,26 @@
+// Last updated: 7/15/2026, 2:07:42 PM
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length())
+        {
+            return false;
+        }
+        int[] freq = new int[26];
+        for(char c : s.toCharArray())
+        {
+            freq[c - 'a'] ++;
+        }
+        for(char c : t.toCharArray())
+        {
+            freq[c - 'a'] --;
+        }
+        for(int count : freq)
+        {
+            if(count != 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
