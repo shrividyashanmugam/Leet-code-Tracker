8c0bb56b2ebc626ee5baaefddd2f671579f607e6
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:06:47 +0530

    Create 595-BigCountries

---
+++ 595-BigCountries/595-BigCountries.sql
@@ -0,0 +1,4 @@
+-- Last updated: 7/15/2026, 2:06:48 PM
+# Write your MySQL query statement below
+# Write your MySQL query statement below
+select name ,population,area from world where area>=3000000 or population>=25000000;
