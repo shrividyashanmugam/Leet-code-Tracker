39fae3a02426bc11a54840f649a0bfe1aba1deed
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:07:35 +0530

    Create 278-FirstBadVersion

---
+++ 278-FirstBadVersion/278-FirstBadVersion.java
@@ -0,0 +1,20 @@
+// Last updated: 7/15/2026, 2:07:35 PM
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+        int left = 1;
+        int right = n;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return left;
+    }
+}
