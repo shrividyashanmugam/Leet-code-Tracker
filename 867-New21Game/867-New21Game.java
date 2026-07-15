87a6b86c5ee38bd07e1e0f1dba443fea7063c128
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:06:22 +0530

    Create 867-New21Game

---
+++ 867-New21Game/867-New21Game.java
@@ -0,0 +1,33 @@
+// Last updated: 7/15/2026, 2:06:22 PM
+class Solution {
+    public double new21Game(int n, int k, int maxPts) {
+
+        if (k == 0 || n >= k - 1 + maxPts) {
+            return 1.0;
+        }
+
+        double[] dp = new double[n + 1];
+
+        dp[0] = 1.0;
+
+        double windowSum = 1.0;
+        double result = 0.0;
+
+        for (int i = 1; i <= n; i++) {
+
+            dp[i] = windowSum / maxPts;
+
+            if (i < k) {
+                windowSum += dp[i];
+            } else {
+                result += dp[i];
+            }
+
+            if (i - maxPts >= 0) {
+                windowSum -= dp[i - maxPts];
+            }
+        }
+
+        return result;
+    }
+}
