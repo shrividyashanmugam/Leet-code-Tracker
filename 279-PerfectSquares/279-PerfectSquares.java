c7f20fd60eaff71a4ac2111d0a4884f54cfdb889
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-15 14:07:32 +0530

    Create 279-PerfectSquares

---
+++ 279-PerfectSquares/279-PerfectSquares.java
@@ -0,0 +1,16 @@
+// Last updated: 7/15/2026, 2:07:33 PM
+class Solution {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i; // Worst case: all 1's
+
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        return dp[n];
+    }
+}
