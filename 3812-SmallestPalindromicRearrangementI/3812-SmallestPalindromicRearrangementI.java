c4672401fcf37065532afa75da5665548c529c36
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-28 21:22:18 +0530

    Create 3812-SmallestPalindromicRearrangementI

---
+++ 3812-SmallestPalindromicRearrangementI/3812-SmallestPalindromicRearrangementI.java
@@ -0,0 +1,25 @@
+// Last updated: 7/28/2026, 9:22:17 PM
+class Solution {
+
+    public String smallestPalindrome(String s) {
+        int partition = s.length() / 2;
+        int[] bucket = new int[26];
+
+        for (int i = 0; i < partition; i++) {
+            bucket[s.charAt(i) - 'a'] += 1;
+        }
+
+        StringBuilder left = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (bucket[i] > 0) {
+                left.append(String.valueOf((char) (i + 'a')).repeat(bucket[i]));
+            }
+        }
+
+        String mid =
+            s.length() % 2 != 0 ? String.valueOf(s.charAt(partition)) : "";
+        String right = new StringBuilder(left).reverse().toString();
+
+        return left.toString() + mid + right;
+    }
+}
