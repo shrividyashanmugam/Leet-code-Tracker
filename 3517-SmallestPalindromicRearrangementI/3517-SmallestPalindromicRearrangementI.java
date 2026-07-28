d511571460921a5c0a5d3a45fb7baa0685b06094
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-28 21:21:55 +0530

    File updated at 7/28/2026, 9:21:53 PM

--- 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.java
+++ 3517-SmallestPalindromicRearrangementI/3517-SmallestPalindromicRearrangementI.java
@@ -1,23 +1,25 @@
-// Last updated: 7/28/2026, 9:21:00 PM
-1import java.util.Arrays;
-2
-3class Solution {
-4    public String smallestPalindrome(String s) {
-5        int n = s.length();
-6        int halfLen = n / 2;
-7        char[] half = s.substring(0, halfLen).toCharArray();
-8        Arrays.sort(half);
-9        
-10        StringBuilder sb = new StringBuilder();
-11        sb.append(half);
-12 
-13        if (n % 2 != 0) {
-14            sb.append(s.charAt(halfLen));
-15        }
-16        for (int i = halfLen - 1; i >= 0; i--) {
-17            sb.append(half[i]);
-18        }
-19        
-20        return sb.toString();
-21    }
-22}
+// Last updated: 7/28/2026, 9:21:53 PM
+class Solution {
+
+    public String smallestPalindrome(String s) {
+        int partition = s.length() / 2;
+        int[] bucket = new int[26];
+
+        for (int i = 0; i < partition; i++) {
+            bucket[s.charAt(i) - 'a'] += 1;
+        }
+
+        StringBuilder left = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            if (bucket[i] > 0) {
+                left.append(String.valueOf((char) (i + 'a')).repeat(bucket[i]));
+            }
+        }
+
+        String mid =
+            s.length() % 2 != 0 ? String.valueOf(s.charAt(partition)) : "";
+        String right = new StringBuilder(left).reverse().toString();
+
+        return left.toString() + mid + right;
+    }
+}
