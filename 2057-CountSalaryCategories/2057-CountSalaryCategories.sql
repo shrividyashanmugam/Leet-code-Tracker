72a38992df43a0a1f3caa5cdea0383906ec38112
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-31 10:16:22 +0530

    Create 2057-CountSalaryCategories

---
+++ 2057-CountSalaryCategories/2057-CountSalaryCategories.sql
@@ -0,0 +1,21 @@
+-- Last updated: 7/31/2026, 10:16:23 AM
+WITH salary_counts AS (
+    SELECT
+        SUM(income < 20000 ) AS low_count,
+        SUM(income BETWEEN 20000 AND 50000 ) AS average_count,
+        SUM(income > 50000 ) AS high_count
+    FROM Accounts
+)
+
+SELECT 'Low Salary' AS category, low_count AS accounts_count
+FROM salary_counts
+
+UNION ALL
+
+SELECT 'Average Salary', average_count
+FROM salary_counts
+
+UNION ALL
+
+SELECT 'High Salary', high_count
+FROM salary_counts;
