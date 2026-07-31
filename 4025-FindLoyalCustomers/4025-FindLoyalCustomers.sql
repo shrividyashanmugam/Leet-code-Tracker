341aef60696ab6811e47a5afdd2275060b61fb51
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-31 10:15:58 +0530

    Create 4025-FindLoyalCustomers

---
+++ 4025-FindLoyalCustomers/4025-FindLoyalCustomers.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/31/2026, 10:15:59 AM
+SELECT customer_id
+FROM customer_transactions
+GROUP BY customer_id
+HAVING 
+    SUM(IF(transaction_type = 'purchase', 1, 0)) >= 3
+    AND DATEDIFF(MAX(transaction_date), MIN(transaction_date)) >= 30
+    AND SUM(IF(transaction_type = 'refund', 1, 0)) / COUNT(*) < 0.20
+ORDER BY customer_id ASC;
