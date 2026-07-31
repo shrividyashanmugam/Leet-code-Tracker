3edc4369e07d138fcd52a1e04377b0f023cc0368
SHRI VIDYA S <shrividyashanmugam4@gmail.com>
2026-07-31 09:18:38 +0530

    Create

---
+++ problem-1785469718039/problem-1785469718039.java
@@ -0,0 +1,19 @@
+// Last updated: 7/31/2026, 9:18:38 AM
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long val=1;
+        row.add((int)val);
+        for(int i=1;i<=rowIndex ;i++){
+           val = val * (rowIndex - i + 1) / i;
+            row.add((int) val);
+        }
+        return row;
+    }
+    // static int fact(int n){
+    //     if(n<=1){
+    //         return 1;
+    //     }
+    //     return n*(fact(n-1));
+    }
+    
